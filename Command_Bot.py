bde563733c041622ef4de9c2d5843dd4fbce754a
Dennis <69431614+SC7RED@users.noreply.github.com>
2020-10-26 23:03:33 -0400

    Create Command_Bot.py

---
+++ Command_Bot.py
@@ -0,0 +1,19 @@
+# command bot.py
+import os
+import random
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='membercount')
+async def membercount(ctx):
+    await ctx.send(len(guild.member_count)
+
+member_count = len([m for m in ctx.guild.members if not m.bot]) # doesn't include bots
+
+bot.run('NzY4Mjg3MzYzOTM5OTU4Nzk3.X4-RXQ.UhbvRg58VlRC9GO1OyKRwT7ewZI')
